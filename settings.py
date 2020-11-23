e3603a712ef2eb9671849113ebf26a1cac0ff31c
Ryan Abdurohman <ryanabdurohman81@gmail.com>
2020-11-23 19:37:11 +0700

    a ship that fires bullets

---
+++ settings.py
@@ -0,0 +1,19 @@
+class Settings:
+    """A Class to store all settings for Alien Invasion."""
+    
+    def __init__(self):
+        """Initialize the game's settings."""
+        #Screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        #ship settings
+        self.ship_speed = 1.5
+
+        #Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
